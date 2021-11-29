c924cac860e4359a12aa1925ff456a1df574b2ea
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-11-29 18:28:51 +0200

    Refactoring

--- examples/bwt.cpp
+++ examples/bwt.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+extern "C" {
+    #include "flbwt.h"
+}
 
-int main() {
-    std::cout << "Hello World!" <<std::endl;
+int main(int argc, char *argv[]) 
+{
+    /* check that user has provided correct number of arguments */
+    if (argc != 3) {
+        std::cout << "Wrong number of arguments!" << std::endl;
+        std::cout << "How to use:" << std::endl;
+        std::cout << "    <program_executable> <input_file> <output_file>" << std::endl;
+        return -1;
+    }
+
+    /* call the burrows-wheeler transform for the input file */
+    bwt_file(argv[1], argv[2]);
+
     return 0;
 }
