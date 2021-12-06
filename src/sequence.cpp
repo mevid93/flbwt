346ff9c2a4422b82551f2f1fe3629708bf0ef6cd
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-12-06 10:35:09 +0200

    Lemma 1 data structure

--- src/sequence.cpp
+++ src/sequence.cpp
@@ -1,39 +1,68 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include "sdsl/util.hpp"
 #include "sequence.hpp"
 #include "utility.hpp"
 
 Sequence::Sequence(const uint64_t n)
 {
     /* allocate memory for the upper stream */
-    uint64_t us_bits = 2 * n;                  
-
-    this->upper_stream = new sdsl::bit_vector(n);
-
+    uint64_t us_bits = 2 * n;
+    this->upper_stream = new sdsl::bit_vector(us_bits, 0);
     this->upper_stream_length = us_bits;
 
     /* allocate memory for the lower stream */
-    uint64_t ls_bits = n * (log2_64(0xffffffffffffffff / n + 1) + 1);
-    uint64_t num_of_int64s = ls_bits / 64 + 1;
-
-    this->lower_stream = (uint64_t *)malloc(num_of_int64s * sizeof(uint64_t));
-
-    if (this->lower_stream == NULL)
-    {
-        throw std::runtime_error("lower_stream malloc failed(): Could not allocate memory");
-    }
-
+    uint64_t ls_bits = n * (log2_64(Sequence::U / n + 1) + 1);
+    this->lower_stream = new sdsl::bit_vector(ls_bits, 0);
     this->lower_stream_length = ls_bits;
+
+    /* set number of integers that can be stored to sequence */
+    this->n = n;
+
+    /* calculate the number of bits considering each stream */
+    uint8_t log2result = log2_64(n);
+
+    this->ubits = log2result;
+    this->lbits = 64 - log2result;
+
+    /* initialize the auxiliary datatype as null */
+    this->sls = NULL;
 }
 
 void Sequence::store_integer(const uint64_t index, const uint64_t integer)
 {
+    this->select_support_enabled = false;
+
+    /* store integer upper stream bits */
+    uint64_t shifted = integer >> (64 - this->ubits);
+    uint64_t us_bit_index = index + shifted;
+    (*this->upper_stream)[us_bit_index] = 1;
+
+    /* store integer lower stream bits */
+    uint64_t ls_index = (index - 1) * this->lbits + 1;
+    this->lower_stream->set_int(ls_index, integer, this->lbits);
 }
 
 uint64_t Sequence::get_integer(const uint64_t index)
 {
-    return 0;
+    if (!this->select_support_enabled)
+    {
+        this->enable_select_operation();
+    }
+
+    /* get upper stream part */
+    uint64_t upper_bits = (*this->sls)(index)-index;
+    upper_bits = upper_bits << (64 - this-> ubits);
+
+    /* get the lower stream part */
+    uint64_t ls_index = (index - 1) * this->lbits + 1;
+    uint64_t lower_bits = this->lower_stream->get_int(ls_index, this->lbits);
+
+    /* combine the parts to form the actual integer */
+    uint64_t integer = upper_bits | lower_bits;
+
+    return integer;
 }
 
 Sequence::~Sequence()
@@ -43,7 +72,25 @@ Sequence::~Sequence()
     this->upper_stream_length = 0;
 
     /* free resources from lower stream */
-    free(this->lower_stream);
-    this->lower_stream = NULL;
+    delete this->lower_stream;
     this->lower_stream_length = 0;
+
+    /* reset the number of integers that can be stored */
+    this->n = 0;
+
+    /* free auxiliary data structure */
+    if (this->sls != NULL)
+    {
+        delete this->sls;
+    }
+
+    /* reset flags */
+    this->select_support_enabled = false;
+}
+
+void Sequence::enable_select_operation()
+{
+    this->select_support_enabled = true;
+
+    this->sls = new sdsl::select_support_mcl<1>(this->upper_stream);
 }
--- test/sequence_test.cpp
+++ test/sequence_test.cpp
@@ -1,32 +1,84 @@
 #include <gtest/gtest.h>
 #include "sequence.hpp"
+#include "utility.hpp"
 
 TEST(sequence_test, construct_sequence_1)
 {
-
+    int n = 15;
+    std::unique_ptr<Sequence> sequence(new Sequence(n));
+    uint8_t EXPECTED_VALUE = 2 * n;
+    EXPECT_EQ(EXPECTED_VALUE, sequence->upper_stream_length);
 }
 
 TEST(sequence_test, construct_sequence_2)
 {
-
+    int n = 1000;
+    std::unique_ptr<Sequence> sequence(new Sequence(n));
+    uint32_t EXPECTED_VALUE = n * (log2_64(0xffffffffffffffff / n + 1) + 1);
+    EXPECT_EQ(EXPECTED_VALUE, sequence->lower_stream_length);
 }
 
 TEST(sequence_test, store_integer_1)
 {
-
+    int n = 15;
+    std::unique_ptr<Sequence> sequence(new Sequence(n));
+    int index = 7;
+    int value = 1584;
+    sequence->store_integer(index, value);
+    uint8_t EXPECTED_VALUE = 1;
+    uint8_t i = 7;
+    EXPECT_EQ(EXPECTED_VALUE, (*sequence->upper_stream)[i]);
 }
 
 TEST(sequence_test, store_integer_2)
 {
-
+    int n = 3;
+    std::unique_ptr<Sequence> sequence(new Sequence(n));
+    sequence->store_integer(3, 333);
+    sequence->store_integer(1, 111);
+    sequence->store_integer(2, 222);
+    EXPECT_EQ(1, (*sequence->upper_stream)[1]);
+    EXPECT_EQ(1, (*sequence->upper_stream)[2]);
+    EXPECT_EQ(1, (*sequence->upper_stream)[3]);
+    uint64_t lbits = 62;
+    EXPECT_EQ(111U, sequence->lower_stream->get_int(1, lbits));
+    EXPECT_EQ(222U, sequence->lower_stream->get_int(64, lbits));
+    EXPECT_EQ(333U, sequence->lower_stream->get_int(127, lbits));
 }
 
 TEST(sequence_test, get_integer_1)
 {
-
+    int n = 15;
+    std::unique_ptr<Sequence> sequence(new Sequence(n));
+    int index = 1;
+    int value = 541;
+    sequence->store_integer(index, value);
+    uint64_t EXPECTED_VALUE = 541;
+    EXPECT_EQ(EXPECTED_VALUE, sequence->get_integer(1));
 }
 
 TEST(sequence_test, get_integer_2)
 {
-
+    int n = 10;
+    std::unique_ptr<Sequence> sequence(new Sequence(n));
+    sequence->store_integer(3, 333);
+    sequence->store_integer(1, 111);
+    sequence->store_integer(2, 222);
+    sequence->store_integer(4, 44444);
+    sequence->store_integer(6, 0xffffffffffffff02);
+    sequence->store_integer(5, 0xffffffffffffff01);
+    sequence->store_integer(8, 0xffffffffffffff04);
+    sequence->store_integer(9, 0xffffffffffffff05);
+    sequence->store_integer(10, 0xfffffffffffffffe);
+    sequence->store_integer(7, 0xffffffffffffff03);
+    EXPECT_EQ(333U, sequence->get_integer(3));
+    EXPECT_EQ(111U, sequence->get_integer(1));
+    EXPECT_EQ(222U, sequence->get_integer(2));
+    EXPECT_EQ(44444U, sequence->get_integer(4));
+    EXPECT_EQ(0xffffffffffffff02, sequence->get_integer(6));
+    EXPECT_EQ(0xffffffffffffff01, sequence->get_integer(5));
+    EXPECT_EQ(0xffffffffffffff04, sequence->get_integer(8));
+    EXPECT_EQ(0xffffffffffffff05, sequence->get_integer(9));
+    EXPECT_EQ(0xfffffffffffffffe, sequence->get_integer(10));
+    EXPECT_EQ(0xffffffffffffff03, sequence->get_integer(7));
 }
