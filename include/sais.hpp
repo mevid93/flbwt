67cbf655fddc766c29b78eb8c69917ea84ed634f
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2022-01-27 14:20:53 +0200

    Switched from sdsl to PackedArray --> slightly faster

--- include/sais.hpp
+++ include/sais.hpp
@@ -2,7 +2,7 @@
 #define FLBWT_SAIS_HPP
 
 #include <stdint.h>
-#include "sdsl/bit_vectors.hpp"
+#include "packed_array.hpp"
 
 /*
  * Modified version of the Yuta Mori's SAIS implementation.
@@ -12,20 +12,17 @@ namespace flbwt
 {
 
     /**
- * @brief Induced sorting for sorting suffixes.
- * 
- * @param T input string
- * @param T_base input string starting offset
- * @param SA suffix array
- * @param SA_signs bit_vector holding information of signs of SA elements
- * @param fs 
- * @param n input string length
- * @param k alphabet size
- * @param T_width T character width in bits
- * @param SA_width SA character widht in bits
- */
-    void sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA, sdsl::bit_vector *SA_signs,
-                   uint64_t fs, uint64_t n, uint64_t k, uint8_t T_width, uint8_t SA_width);
+     * @brief Induced sorting for sorting suffixes.
+     * 
+     * @param T input string
+     * @param T_base input string starting offset
+     * @param SA suffix array
+     * @param fs 
+     * @param n input string length
+     * @param k alphabet size
+     */
+    void sais_main(flbwt::PackedArray *T, uint64_t T_base, flbwt::PackedArray *SA,
+                   uint64_t fs, uint64_t n, uint64_t k);
 
 }
 
--- src/sais.cpp
+++ src/sais.cpp
@@ -2,67 +2,23 @@
 #include "sais.hpp"
 #include "utility.hpp"
 
-// get value from B/C-vector at some index
-static int64_t BC_get_value(sdsl::bit_vector *BC, uint64_t BC_base, int64_t index, uint8_t BC_width)
-{
-    return BC->get_int(BC_base + index * BC_width, BC_width);
-}
-
-// set value in B/C-vector at some index
-static void BC_set_value(sdsl::bit_vector *BC, uint64_t BC_base, int64_t index, uint8_t BC_width, uint64_t value)
-{
-    BC->set_int(BC_base + index * BC_width, value, BC_width);
-}
-
-// get value from T-vector at index + 1
-static uint64_t T_get_value(sdsl::bit_vector *T, uint64_t T_base, int64_t index, uint8_t T_width)
-{
-    return T->get_int(T_base + (index + 1) * T_width, T_width);
-}
-
-// get value from SA at some index
-static int64_t SA_get_value(sdsl::bit_vector *SA, sdsl::bit_vector *SA_signs, uint64_t index, uint8_t SA_width)
-{
-    int64_t value = SA->get_int(index * SA_width, SA_width);
-    if ((*SA_signs)[index] == 1) // sign bit set --> negative value
-        value = -value;
-    return value;
-}
-
-// set value in SA-vector at some index
-static void SA_set_value(sdsl::bit_vector *SA, sdsl::bit_vector *SA_signs, uint64_t index, uint8_t SA_width, int64_t value)
-{
-    if (value < 0)
-    {
-        (*SA_signs)[index] = 1; // set negative sign bit
-        value = -value;
-    }
-    else
-    {
-        (*SA_signs)[index] = 0;
-    }
-
-    SA->set_int(index * SA_width, value, SA_width);
-}
-
 // get the total count of each alphabet character in the input string
-static void get_counts(sdsl::bit_vector *T, uint64_t T_base, uint8_t T_width, sdsl::bit_vector *C, uint64_t C_base,
-                       uint8_t C_width, uint64_t n, uint64_t k)
+static void get_counts(flbwt::PackedArray *T, uint64_t T_base, flbwt::PackedArray *C, uint64_t C_base, uint64_t n, uint64_t k)
 {
     for (uint64_t i = 0; i < k; i++)
     {
-        BC_set_value(C, C_base, i, C_width, 0);
+        C->set_value(C_base + i, 0);
     }
     for (uint64_t i = 0; i < n; i++)
     {
-        uint64_t idx = T_get_value(T, T_base, i, T_width);
-        uint64_t old_count = BC_get_value(C, C_base, idx, C_width);
-        BC_set_value(C, C_base, idx, C_width, old_count + 1);
+        uint64_t idx = T->get_value(T_base + i + 1);
+        uint64_t old_count = C->get_value(C_base + idx);
+        C->set_value(C_base + idx, old_count + 1);   
     }
 }
 
 // find the start or end of each bucket
-static void get_buckets(sdsl::bit_vector *C, uint64_t C_base, sdsl::bit_vector *B, uint64_t B_base, uint64_t k, bool end, uint8_t BC_width)
+static void get_buckets(flbwt::PackedArray *C, uint64_t C_base, flbwt::PackedArray *B, uint64_t B_base, uint64_t k, bool end)
 {
     uint64_t sum = 0;
 
@@ -70,25 +26,24 @@ static void get_buckets(sdsl::bit_vector *C, uint64_t C_base, sdsl::bit_vector *
     {
         for (uint64_t i = 0; i < k; i++)
         {
-            sum += BC_get_value(C, C_base, i, BC_width);
-            BC_set_value(B, B_base, i, BC_width, sum);
+            sum += C->get_value(C_base + i);
+            B->set_value(B_base + i, sum);
         }
     }
     else
     {
         for (uint64_t i = 0; i < k; i++)
         {
-            uint64_t val = BC_get_value(C, C_base, i, BC_width);
+            uint64_t val = C->get_value(C_base + i);
             sum += val;
-            BC_set_value(B, B_base, i, BC_width, sum - val);
+            B->set_value(B_base + i, sum - val);
         }
     }
 }
 
 // compute SA
-static void induce_SA(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA, sdsl::bit_vector *SA_signs, sdsl::bit_vector *C,
-                      uint64_t C_base, sdsl::bit_vector *B, uint64_t B_base, uint64_t n, uint64_t k, uint8_t T_width,
-                      uint8_t BC_width, uint8_t SA_width)
+static void induce_SA(flbwt::PackedArray *T, uint64_t T_base, flbwt::PackedArray *SA, flbwt::PackedArray *C, uint64_t C_base,
+                      flbwt::PackedArray *B, uint64_t B_base, uint64_t n, uint64_t k)
 {
     int64_t j;
     int64_t c0;
@@ -98,37 +53,37 @@ static void induce_SA(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
     // compute SA1
     if (C_base == B_base)
     {
-        get_counts(T, T_base, T_width, C, C_base, BC_width, n, k);
+        get_counts(T, T_base, C, C_base, n, k);
     }
 
-    get_buckets(C, C_base, B, B_base, k, false, BC_width);
+    get_buckets(C, C_base, B, B_base, k, false);
 
     j = n - 1;
-    c1 = T_get_value(T, T_base, j, T_width);
-    b = BC_get_value(B, B_base, c1, BC_width);
+    c1 = T->get_value(T_base + j + 1);
+    b = B->get_value(B_base + c1);
 
-    int64_t value = ((0 < j) && ((int64_t)T_get_value(T, T_base, j - 1, T_width) < c1)) ? ~j : j;
-    SA_set_value(SA, SA_signs, b, SA_width, value);
+    int64_t value = ((0 < j) && (T->get_value(T_base + j) < c1)) ? ~j : j;
+    SA->set_value(b, value);
     b++;
 
     for (uint64_t i = 0; i < n; i++)
     {
-        j = SA_get_value(SA, SA_signs, i, SA_width);
-        SA_set_value(SA, SA_signs, i, SA_width, ~j);
+        j = SA->get_value(i);
+        SA->set_value(i, ~j);
 
         if (0 < j)
         {
             j--;
-            c0 = T_get_value(T, T_base, j, T_width);
+            c0 = T->get_value(T_base + j + 1);
             if (c0 != c1)
             {
-                BC_set_value(B, B_base, c1, BC_width, b);
+                B->set_value(B_base + c1, b);
                 c1 = c0;
-                b = BC_get_value(B, B_base, c1, BC_width);
+                b = B->get_value(B_base + c1);
             }
 
-            value = ((0 < j) && ((int64_t)T_get_value(T, T_base, j - 1, T_width) < c1)) ? ~j : j;
-            SA_set_value(SA, SA_signs, b, SA_width, value);
+            value = ((0 < j) && (T->get_value(T_base + j) < c1)) ? ~j : j;
+            SA->set_value(b, value);
             b++;
         }
     }
@@ -136,48 +91,46 @@ static void induce_SA(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
     // compute the SAs
     if (C_base == B_base)
     {
-        get_counts(T, T_base, T_width, C, C_base, BC_width, n, k);
+        get_counts(T, T_base, C, C_base, n, k);
     }
 
-    get_buckets(C, C_base, B, B_base, k, true, BC_width);
+    get_buckets(C, C_base, B, B_base, k, true);
 
     c1 = 0;
-    b = BC_get_value(B, B_base, c1, BC_width);
+    b = B->get_value(B_base + c1);
     for (int64_t i = n - 1; 0 <= i; i--)
     {
-        j = SA_get_value(SA, SA_signs, i, SA_width);
+        j = SA->get_value(i);
 
         if (0 < j)
         {
             j--;
-            c0 = T_get_value(T, T_base, j, T_width);
+            c0 = T->get_value(T_base + j + 1);
             if (c0 != c1)
             {
-                BC_set_value(B, B_base, c1, BC_width, b);
+                B->set_value(B_base + c1, b);
                 c1 = c0;
-                b = BC_get_value(B, B_base, c1, BC_width);
+                b = B->get_value(B_base + c1);
             }
 
-            value = ((j == 0) || ((int64_t)T_get_value(T, T_base, j - 1, T_width) > c1)) ? ~j : j;
+            value = ((j == 0) || (T->get_value(T_base + j) > c1)) ? ~j : j;
             b--;
-            SA_set_value(SA, SA_signs, b, SA_width, value);
+            SA->set_value(b, value);
         }
         else
         {
-            SA_set_value(SA, SA_signs, i, SA_width, ~j);
+            SA->set_value(i, ~j);
         }
     }
 }
 
-void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA, sdsl::bit_vector *SA_signs,
-                      uint64_t fs, uint64_t n, uint64_t k, uint8_t T_width, uint8_t SA_width)
+void flbwt::sais_main(flbwt::PackedArray *T, uint64_t T_base, flbwt::PackedArray *SA, uint64_t fs, uint64_t n, uint64_t k)
 {
-    sdsl::bit_vector *C;  // pointer to C-vector
-    sdsl::bit_vector *B;  // pointer to B-vector
-    sdsl::bit_vector *BC; // pointer to BC-vector
-    uint64_t C_base;      // start offset of C
-    uint64_t B_base;      // start offset of B
-    uint8_t BC_width;     // width of the numbers in C and B vectors
+    flbwt::PackedArray *C;  // pointer to C-vector
+    flbwt::PackedArray *B;  // pointer to B-vector
+    flbwt::PackedArray *BC; // pointer to BC-vector
+    uint64_t C_base;        // start offset of C
+    uint64_t B_base;        // start offset of B
     int64_t c;
     int64_t c0;
     int64_t c1;
@@ -195,44 +148,42 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
     if (k <= fs)
     {
         C = B = SA;
-        C_base = n * SA_width;
-        B_base = (k <= (fs - k)) ? C_base + k * SA_width : C_base;
-        BC_width = SA_width;
+        C_base = n;
+        B_base = (k <= (fs - k)) ? C_base + k : C_base;
     }
     else
     {
-        BC_width = flbwt::position_of_msb(n);
-        BC = new sdsl::bit_vector(k * BC_width, 0);
+        BC = new flbwt::PackedArray(k, n, false);
         C = B = BC;
         C_base = B_base = 0;
     }
 
-    get_counts(T, T_base, T_width, C, C_base, BC_width, n, k);
-    get_buckets(C, C_base, B, B_base, k, true, BC_width);
+    get_counts(T, T_base, C, C_base, n, k);
+    get_buckets(C, C_base, B, B_base, k, true);
 
     for (uint64_t i = 0; i < n; i++)
-        SA_set_value(SA, SA_signs, i, SA_width, 0);
+        SA->set_value(i, 0);
 
     c = 0;
-    c1 = T_get_value(T, T_base, n - 1, T_width);
+    c1 = T->get_value(T_base + n);
     for (int64_t i = n - 2; 0 <= i; i--)
     {
-        if ((c0 = T_get_value(T, T_base, i, T_width)) < (c1 + c))
+        if ((c0 = T->get_value(T_base + i + 1)) < (c1 + c))
         {
             c = 1;
         }
         else if (c != 0)
         {
-            uint64_t index = BC_get_value(B, B_base, c1, BC_width) - 1;
-            BC_set_value(B, B_base, c1, BC_width, index);
-            SA_set_value(SA, SA_signs, index, SA_width, i + 1);
+            uint64_t index = B->get_value(B_base + c1) - 1;
+            B->set_value(B_base + c1, index);
+            SA->set_value(index, i + 1);
             c = 0;
         }
 
         c1 = c0;
     }
 
-    induce_SA(T, T_base, SA, SA_signs, C, C_base, B, B_base, n, k, T_width, BC_width, SA_width);
+    induce_SA(T, T_base, SA, C, C_base, B, B_base, n, k);
 
     if (fs < k)
     {
@@ -243,42 +194,42 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
     m = 0;
     for (uint64_t i = 0; i < n; i++)
     {
-        p = SA_get_value(SA, SA_signs, i, SA_width);
+        p = SA->get_value(i);
 
         if (0 < p)
         {
-            int64_t prev = T_get_value(T, T_base, p - 1, T_width);
-            c0 = T_get_value(T, T_base, p, T_width);
+            int64_t prev = T->get_value(T_base + p);
+            c0 = T->get_value(T_base + p + 1);
 
             if (prev > c0)
             {
                 for (j = p + 1; (j < (int64_t)n); j++)
                 {
-                    c1 = T_get_value(T, T_base, j, T_width);
+                    c1 = T->get_value(T_base + j + 1);
                     if (c0 != c1)
                         break;
                 }
 
                 if ((j < (int64_t)n) && (c0 < c1))
                 {
-                    SA_set_value(SA, SA_signs, m, SA_width, p);
+                    SA->set_value(m, p);
                     m++;
                 }
             }
         }
     }
-    
+
     // int the name array buffer
     for (uint64_t i = m; i < n; i++)
-        SA_set_value(SA, SA_signs, i, SA_width, 0);
+        SA->set_value(i, 0);
 
     // store the length of all substrings
     j = n;
     c = 0;
-    c1 = T_get_value(T, T_base, n - 1, T_width);
+    c1 = T->get_value(T_base + n);
     for (int64_t i = n - 2; i >= 0; i--, c1 = c0)
     {
-        c0 = T_get_value(T, T_base, i, T_width);
+        c0 = T->get_value(T_base + i + 1);
 
         if (c0 < c1 + c)
         {
@@ -286,8 +237,7 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
         }
         else if (c != 0)
         {
-            int64_t index = m + ((i + 1) >> 1);
-            SA_set_value(SA, SA_signs, index, SA_width, j - i - 1);
+            SA->set_value(m + ((i + 1) >> 1), j - i - 1);
             j = i + 1;
             c = 0;
         }
@@ -299,17 +249,17 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
     qlen = 0;
     for (int64_t i = 0; i < m; i++)
     {
-        p = SA_get_value(SA, SA_signs, i, SA_width);
+        p = SA->get_value(i);
         int64_t index = m + (p >> 1);
-        plen = SA_get_value(SA, SA_signs, index, SA_width);
+        plen = SA->get_value(index);
         diff = 1;
 
         if (plen == qlen)
         {
             for (j = 0; j < plen; j++)
             {
-                uint64_t pch = T_get_value(T, T_base, p + j, T_width);
-                uint64_t qch = T_get_value(T, T_base, q + j, T_width);
+                uint64_t pch = T->get_value(T_base + p + j + 1);
+                uint64_t qch = T->get_value(T_base + q + j + 1);
                 if (pch != qch)
                     break;
             }
@@ -325,7 +275,7 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
             qlen = plen;
         }
 
-        SA_set_value(SA, SA_signs, index, SA_width, name);
+        SA->set_value(index, name);
     }
 
     // STAGE 2: Solve the reduced problem. Recurse if names are not yet unique
@@ -334,27 +284,27 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
         RA = n + fs - m;
         for (int64_t i = n - 1, j = m - 1; m <= i; i--)
         {
-            int64_t value = SA_get_value(SA, SA_signs, i, SA_width);
+            int64_t value = SA->get_value(i);
 
             if (value != 0)
             {
-                SA_set_value(SA, SA_signs, RA + j, SA_width, value - 1);
+                SA->set_value(RA + j, value - 1);
                 j--;
             }
         }
 
-        flbwt::sais_main(SA, (RA - 1) * SA_width, SA, SA_signs, fs + n - m * 2, m, name, SA_width, SA_width);
+        flbwt::sais_main(SA, (RA - 1), SA, fs + n - m * 2, m, name);
 
-        for (int64_t i = n - 2, j = m - 1, c = 0, c1 = T_get_value(T, T_base, n - 1, T_width); 0 <= i; i--, c1 = c0)
+        for (int64_t i = n - 2, j = m - 1, c = 0, c1 = T->get_value(T_base + n); 0 <= i; i--, c1 = c0)
         {
-            c0 = T_get_value(T, T_base, i, T_width);
+            c0 = T->get_value(T_base + i + 1);
             if (c0 < c1 + c)
             {
                 c = 1;
             }
             else if (c != 0)
             {
-                SA_set_value(SA, SA_signs, RA + j, SA_width, i + 1);
+                SA->set_value(RA + j, i + 1);
                 j--;
                 c = 0;
             }
@@ -362,9 +312,9 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
 
         for (int64_t i = 0; i < m; i++)
         {
-            int64_t index = SA_get_value(SA, SA_signs, i, SA_width);
-            int64_t value = SA_get_value(SA, SA_signs, RA + index, SA_width);
-            SA_set_value(SA, SA_signs, i, SA_width, value);
+            int64_t index = SA->get_value(i);
+            int64_t value = SA->get_value(RA + index);
+            SA->set_value(i, value);
         }
     }
 
@@ -372,40 +322,39 @@ void flbwt::sais_main(sdsl::bit_vector *T, uint64_t T_base, sdsl::bit_vector *SA
     if (k <= fs)
     {
         C = B = SA;
-        C_base = n * SA_width;
-        B_base = (k <= (fs - k)) ? C_base + k * SA_width : C_base;
-        BC_width = SA_width;
+        C_base = n;
+        B_base = (k <= (fs - k)) ? C_base + k: C_base;
     }
     else
     {
-        BC_width = flbwt::position_of_msb(n);
-        BC = new sdsl::bit_vector(k * BC_width, 0);
+        BC = new PackedArray(k, n, false);
         C = B = BC;
         C_base = B_base = 0;
     }
 
     // put all LMS characters into their buckets
-    get_counts(T, T_base, T_width, C, C_base, BC_width, n, k);
-    get_buckets(C, C_base, B, B_base, k, true, BC_width);
+    get_counts(T, T_base, C, C_base, n, k);
+    get_buckets(C, C_base, B, B_base, k, true);
 
     for (uint64_t i = m; i < n; i++)
-       SA_set_value(SA, SA_signs, i, SA_width, 0);
+        SA->set_value(i, 0);
 
     for (int64_t i = m - 1; 0 <= i; i--)
     {
-       j = SA_get_value(SA, SA_signs, i, SA_width);
-       SA_set_value(SA, SA_signs, i, SA_width, 0);
+        j = SA->get_value(i);
+        SA->set_value(i, 0);
 
-       int64_t index = T_get_value(T, T_base, j, T_width);
-       int64_t value = BC_get_value(B, B_base, index, BC_width) - 1;
-       BC_set_value(B, B_base, index, BC_width, value);
-       SA_set_value(SA, SA_signs, value, SA_width, j);
+        int64_t index = T->get_value(T_base + j + 1);
+        int64_t value = B->get_value(B_base + index ) - 1;
+        B->set_value(B_base + index, value);
+        SA->set_value(value, j);
     }
 
-    induce_SA(T, T_base, SA, SA_signs, C, C_base, B, B_base, n, k, T_width, BC_width, SA_width);
+    induce_SA(T, T_base, SA, C, C_base, B, B_base, n, k);
 
     if (fs < k)
     {
         delete BC;
     }
+
 }
